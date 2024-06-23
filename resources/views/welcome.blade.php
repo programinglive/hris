15fcd04bff364944b39307677f585a29550f2a96
programinglive <mahatmamahardhika200588@gmail.com>
2024-06-23 19:06:30 +0700

    chore: Add postcss and tailwind config files, and update dependencies in yarn.lock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,24 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
-    <body>
-        <h1>HRIS Project</h1>
-    
+    <body class="bg-blue-50">
+        <header class="text-center bg-blue-500 text-white p-6">
+            <h1 class="font-bold text-xl">Welcome to HRIS By ProgramingLive Community</h1>
+        </header>
+        <main class="max-w-xl mx-auto p-4 md:p-8 my-5 rounded-md bg-white shadow-2xl">
+            <section class="text-center">
+                <h3 class="font-bold text-2xl">Your Comprehensive HR Solution</h3>
+                <p class="text-gray-600 mt-2">Manage your company's human resources efficiently with our user-friendly interface and powerful features.</p>
+            </section>
+            <section class="mt-5">
+                <div class="flex flex-col items-center">
+                    <a href="#" class="w-full px-4 py-2 rounded-md text-center text-white bg-blue-500 hover:bg-blue-700 transition-colors mb-4 max-w-xs">Log in</a>
+                    <a href="#" class="w-full px-4 py-2 rounded-md text-center text-blue-500 border border-blue-500 hover:bg-blue-50 transition-colors max-w-xs">Register</a>
+                </div>
+            </section>
+        </main>
+        <footer class="text-center text-blue-600 mt-8">
+            <p>&copy; 2024 ProgramingLive Community | All rights reserved.</p>
+        </footer>
     </body>
 </html>
