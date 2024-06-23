5476d994de1f61d9db261ef9adc42fb542725299
programinglive <mahatmamahardhika200588@gmail.com>
2024-06-23 21:28:09 +0700

    feat: Add markdown rendering and tweak interface
    Implemented Shiki-based markdown rendering via a new spatie/laravel-markdown package. Made several changes to component layouts and styles for improved visual appearance, including modifications to page headers, main content, and landing page layouts. Dependency updates were made in composer.json and package.json for this integration.

--- resources/views/livewire/main-content.blade.php
+++ resources/views/livewire/main-content.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="container mx-auto">
+	<x-markdown class="flex flex-col gap-2 p-5">
+	{{ file_get_contents(base_path('README.md')) }}
+	</x-markdown>
 </div>
+
+@push('styles')
+	<style>
+		h1, h2, h3, h4, h5, h6{
+				font-weight: bold;
+		}
+	</style>
+@endpush
--- resources/views/livewire/page-header.blade.php
+++ resources/views/livewire/page-header.blade.php
@@ -1,5 +1,5 @@
-<div>
-	<nav class="w-full flex flex-col lg:flex-row justify-between bg-blue-500 p-6">
+<div class="container mx-auto">
+	<nav class="w-full flex flex-col lg:flex-row justify-between bg-blue-500 p-6 shadow-md">
 		<div class="flex items-center lg:items-start flex-shrink-0 text-white lg:mb-0">
 			<a href="#" class="flex align-items-center">
 				<img src="{{ asset('assets/images/hris_logo.svg') }}" alt="Logo" class="h-8 w-auto">
