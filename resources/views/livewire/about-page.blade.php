b5949c7db5b611015465ed784d60eb7193f4c7aa
Mahatma Mahardhika <mahatma.mahardhika@programinglive.com>
2024-07-01 02:08:09 +0700

    chore(livewire-about-page): add detailed about us content
    Replaced blank div with detailed section about the company in about-page. Content includes company's mission, vision and values.

--- resources/views/livewire/about-page.blade.php
+++ resources/views/livewire/about-page.blade.php
@@ -1,3 +1,49 @@
-<div>
-    
+<div class="flex flex-col gap-3">
+	<livewire:page-header/>
+	
+	<main>
+		
+		<main>
+			<section class="px-6 py-8 prose max-w-7xl mx-auto">
+				<h1 class="text-4xl font-semibold text-center text-gray-800 dark:text-white">
+					About Us
+				</h1>
+				
+				<p class="text-lg text-center text-gray-600 dark:text-gray-400 mb-10">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, commodi?
+				</p>
+				
+				<div class="flex flex-wrap items-center justify-around">
+					<div class="w-full sm:w-1/2 lg:w-1/4 p-6 flex flex-col">
+						<img class="h-48 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="">
+						<div class="flex-grow">
+							<h2 class="text-gray-900 text-lg title-font font-medium mb-3">Our Vision</h2>
+							<p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+								voluptatem nemo facilis cum, maxime?</p>
+						</div>
+					</div>
+					
+					<div class="w-full sm:w-1/2 lg:w-1/4 p-6 flex flex-col">
+						<img class="h-48 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="">
+						<div class="flex-grow">
+							<h2 class="text-gray-900 text-lg title-font font-medium mb-3">Our Mission</h2>
+							<p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+								voluptatem nemo facilis cum, maxime?</p>
+						</div>
+					</div>
+					
+					<div class="w-full sm:w-1/2 lg:w-1/4 p-6 flex flex-col">
+						<img class="h-48 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="">
+						<div class="flex-grow">
+							<h2 class="text-gray-900 text-lg title-font font-medium mb-3">Our Values</h2>
+							<p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+								voluptatem nemo facilis cum, maxime?</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+	</main>
+	
+	<livewire:page-footer/>
 </div>
