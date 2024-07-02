14a3dfca4bab260f2a655832a7a939461c31c390
Mahatma Mahardhika <mahatma.mahardhika@programinglive.com>
2024-07-02 23:09:13 +0700

    test(route-test): added @test annotations and refined method names

--- tests/Unit/RouteTest.php
+++ tests/Unit/RouteTest.php
@@ -10,19 +10,25 @@ class RouteTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * @return void
+     * @test
      */
-    public function test_it_check_home_route()
+    public function it_check_home_route()
     {
         $this->get(route('home'))->assertStatus(200);
     }
 
-    public function test_it_check_about_route()
+    /**
+     * @test
+     */
+    public function it_check_about_route()
     {
         $this->get(route('about'))->assertStatus(200);
     }
 
-    public function test_it_check_docs_route()
+    /**
+     * @test
+     */
+    public function it_check_docs_route()
     {
         $this->get(route('docs'))->assertStatus(200);
     }
