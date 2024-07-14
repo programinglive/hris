1ba3851c823425e02ff3549fa9c057a0b09e5ede
Mahatma Mahardhika <mahatma.mahardhika@programinglive.com>
2024-07-14 23:54:50 +0700

    chore(resources-views): remove kanban view and update sidebar blade
    This commit removes the kanban view from the application and updates the sidebar blade layout. The `kanban.blade.php` file has been deleted, and `sidebar.blade.php` has been revised to include additional components and refactor the layout structure.

--- resources/views/layouts/shared/sidebar.blade.php
+++ resources/views/layouts/shared/sidebar.blade.php
@@ -1,223 +1,126 @@
 <div class="app-menu">
-
-    <!-- Sidenav Brand Logo -->
-    <a href="#" class="logo-box">
-        <!-- Light Brand Logo -->
-        <div class="logo-light">
-            <img src="{{ asset('/images/logo-light.png') }}" class="logo-lg h-6" alt="Light logo">
-            <img src="{{ asset('/images/logo-light.png') }}" class="logo-sm" alt="Small logo">
-        </div>
-
-        <!-- Dark Brand Logo -->
-        <div class="logo-dark">
-            <img src="{{ asset('/images/logo-dark.png') }}" class="logo-lg h-6" alt="Dark logo">
-            <img src="{{ asset('/images/logo-light.png') }}" class="logo-sm" alt="Small logo">
-        </div>
-    </a>
-
-    <!-- Sidenav Menu Toggle Button -->
-    <button id="button-hover-toggle" class="absolute top-5 end-2 rounded-full p-1.5">
-        <span class="sr-only">Menu Toggle Button</span>
-        <i class="mgc_round_line text-xl"></i>
-    </button>
-
-    <!--- Menu -->
-    <div class="srcollbar" data-simplebar>
-        <ul class="menu" data-fc-type="accordion">
-            <li class="menu-title">Menu</li>
-
-            <li class="menu-item">
-                <a href="#" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                    <span class="menu-text"> Dashboard </span>
-                </a>
-            </li>
-
-            <li class="menu-title">Apps</li>
-
-            <li class="menu-item">
-                <a href="#" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_calendar_line"></i></span>
-                    <span class="menu-text"> Calendar </span>
-                </a>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_coupon_line"></i></span>
-                    <span class="menu-text"> Tickets </span>
-                </a>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_folder_2_line"></i></span>
-                    <span class="menu-text"> File Manager </span>
-                </a>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_task_2_line"></i></span>
-                    <span class="menu-text">Kanban</span>
-                </a>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_building_2_line"></i></span>
-                    <span class="menu-text"> Project </span>
-                    <span class="menu-arrow"></span>
-                </a>
-
-                <ul class="sub-menu hidden">
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">List</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Detail</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Create</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="menu-title">Custom</li>
-
-            <li class="menu-item">
-                <a href="#" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_user_3_line"></i></span>
-                    <span class="menu-text"> Auth Pages </span>
-                    <span class="menu-arrow"></span>
-                </a>
-
-                <ul class="sub-menu hidden">
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Log In</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Register</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Recover Password</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Lock Screen</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_box_3_line"></i></span>
-                    <span class="menu-text"> Extra Pages </span>
-                    <span class="menu-arrow"></span>
-                </a>
-
-            </li>
-
-            <li class="menu-title">Elements</li>
-
-            <li class="menu-item">
-                <a href="#" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_layout_grid_line"></i></span>
-                    <span class="menu-text"> Tables </span>
-                    <span class="menu-arrow"></span>
-                </a>
-
-                <ul class="sub-menu hidden">
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Basic Tables</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Data Tables</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_dribbble_line"></i></span>
-                    <span class="menu-text"> Icons </span>
-                    <span class="menu-arrow"></span>
-                </a>
-
-                <ul class="sub-menu hidden">
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Mingcute</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Feather</span>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Material Symbols </span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_chart_bar_line"></i></span>
-                    <span class="menu-text"> Chart </span>
-                </a>
-            </li>
-
-            <li class="menu-item">
-                <a href="#" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_location_line"></i></span>
-                    <span class="menu-text"> Maps </span>
-                    <span class="menu-arrow"></span>
-                </a>
-
-                <ul class="sub-menu hidden">
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Google Maps</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-
-        <!-- Help Box Widget -->
-        <div class="my-10 mx-5">
-            <div class="help-box p-6 bg-black/5 text-center rounded-md">
-                <div class="flex justify-center mb-4">
-                    <svg width="30" height="18" aria-hidden="true">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M15 0c-4 0-6.5 2-7.5 6 1.5-2 3.25-2.75 5.25-2.25 1.141.285 1.957 1.113 2.86 2.03C17.08 7.271 18.782 9 22.5 9c4 0 6.5-2 7.5-6-1.5 2-3.25 2.75-5.25 2.25-1.141-.285-1.957-1.113-2.86-2.03C20.42 1.728 18.718 0 15 0ZM7.5 9C3.5 9 1 11 0 15c1.5-2 3.25-2.75 5.25-2.25 1.141.285 1.957 1.113 2.86 2.03C9.58 16.271 11.282 18 15 18c4 0 6.5-2 7.5-6-1.5 2-3.25 2.75-5.25 2.25-1.141-.285-1.957-1.113-2.86-2.03C12.92 10.729 11.218 9 7.5 9Z"
-                            fill="#38BDF8"></path>
-                    </svg>
-                </div>
-                <h5 class="mb-2">Unlimited Access</h5>
-                <p class="mb-3">Upgrade to plan to get access to unlimited reports</p>
-                <a href="#" class="btn btn-sm bg-secondary text-white">Upgrade</a>
-            </div>
-        </div>
-    </div>
+	
+	<!-- Sidenav Brand Logo -->
+	<a href="#" class="logo-box flex space-x-2">
+		<!-- Light Brand Logo -->
+		<div class="logo-light">
+			<img src="{{ asset('/assets/images/hris_logo.svg') }}" class="logo-lg h-6" alt="Light logo">
+			<img src="{{ asset('/assets/images/hris_logo.svg') }}" class="logo-sm" alt="Small logo">
+		</div>
+		
+		<!-- Dark Brand Logo -->
+		<div class="logo-dark">
+			<img src="{{ asset('/assets/images/hris_logo.svg') }}" class="logo-lg h-6" alt="Dark logo">
+			<img src="{{ asset('/assets/images/hris_logo.svg') }}" class="logo-sm" alt="Small logo">
+		</div>
+		
+		<div>HRIS</div>
+	</a>
+	
+	<!-- Sidenav Menu Toggle Button -->
+	<button id="button-hover-toggle" class="absolute top-5 end-2 rounded-full p-1.5">
+		<span class="sr-only">Menu Toggle Button</span>
+		<i class="mgc_round_line text-xl"></i>
+	</button>
+	
+	<!--- Menu -->
+	<div class="scrollbar" data-simplebar>
+		<ul class="menu" data-fc-type="accordion">
+			<li class="menu-title">Menu</li>
+			
+			<li class="menu-item">
+				<a href="#" class="menu-link">
+					<span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+					<span class="menu-text"> Dashboard </span>
+				</a>
+			</li>
+			
+			<li class="menu-title">Master Data</li>
+			
+			<li class="menu-item">
+				<a href="#" data-fc-type="collapse" class="menu-link">
+					<span class="menu-icon"><i class="mgc_building_2_line"></i></span>
+					<span class="menu-text"> Employee </span>
+					<span class="menu-arrow"></span>
+				</a>
+				
+				<ul class="sub-menu hidden">
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">List</span>
+						</a>
+					</li>
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Detail</span>
+						</a>
+					</li>
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Create</span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			
+			<li class="menu-title">Transaction Data</li>
+			
+			<li class="menu-item">
+				<a href="#" data-fc-type="collapse" class="menu-link">
+					<span class="menu-icon"><i class="mgc_user_3_line"></i></span>
+					<span class="menu-text"> Auth Pages </span>
+					<span class="menu-arrow"></span>
+				</a>
+				
+				<ul class="sub-menu hidden">
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Log In</span>
+						</a>
+					</li>
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Register</span>
+						</a>
+					</li>
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Recover Password</span>
+						</a>
+					</li>
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Lock Screen</span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			
+			<li class="menu-title">Settings</li>
+			
+			<li class="menu-item">
+				<a href="#" data-fc-type="collapse" class="menu-link">
+					<span class="menu-icon"><i class="mgc_layout_grid_line"></i></span>
+					<span class="menu-text"> Tables </span>
+					<span class="menu-arrow"></span>
+				</a>
+				
+				<ul class="sub-menu hidden">
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Basic Tables</span>
+						</a>
+					</li>
+					<li class="menu-item">
+						<a href="#" class="menu-link">
+							<span class="menu-text">Data Tables</span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			
+		</ul>
+		
+	</div>
 </div>
 <!-- Sidenav Menu End  -->
