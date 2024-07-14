823368bb5a85eebdfe432505cc3c1f034c0d2a02
Mahatma Mahardhika <mahatma.mahardhika@programinglive.com>
2024-07-14 14:24:55 +0700

    chore(app-blade): update title to use app name from config

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="utf-8">
-	<title>{{ $title ?? '' }} | Beautyworld HRIS</title>
+	<title>{{ $title ?? '' }} | {{ config('app.name') }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta content="Beautyworld HRIS" name="description">
 	<meta content="Beautyworld Team" name="author">
