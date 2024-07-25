80ec47a1e7c8abd82a4df94199c17a407441bb8c
Mahatma Mahardhika <mahatma.mahardhika@programinglive.com>
2024-07-25 17:41:41 +0700

    chore: tidy up

--- resources/views/livewire/dashboard-page.blade.php
+++ resources/views/livewire/dashboard-page.blade.php
@@ -1,4 +1,4 @@
-<div class="relative w-full h-screen bg-cover bg-center" style="background-image: url({{ asset('hiring_1.jpg') }});">
+<div class="relative w-full h-screen bg-cover bg-center" style="background-image: url('{{ asset('hiring_1.jpg') }}');">
 	<div class="absolute inset-0 bg-black opacity-50"></div>
 	<div class="relative z-10 flex items-center justify-center h-full text-white">
 		<h1 class="text-4xl font-bold">We Developed Beautiful HRIS Application</h1>
