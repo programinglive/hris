cac4da1e553d71cef0635bf3ad88f9e5f71092f9
Mahatma Mahardhika <mahatma.mahardhika@programinglive.com>
2024-07-30 02:30:06 +0700

    feat(web): update branch table to use optional chaining
    Updated the branch table in the livewire component to use optional chaining for the company name to prevent potential errors.

--- resources/views/livewire/branch-table.blade.php
+++ resources/views/livewire/branch-table.blade.php
@@ -37,7 +37,7 @@
 							<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500">
 								<div class="flex justify-between">
 									<div>Company</div>
-									<div>{{ $branch->company->name }}</div>
+									<div>{{ $branch->company?->name }}</div>
 								</div>
 								<div class="flex justify-between">
 									<div>Code</div>
