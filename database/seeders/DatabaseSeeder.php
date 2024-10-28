9aac1568c96debb0856a1ca98e720987d18d1e3a
Mahatma Mahardhika <mahatmamahardhika200588@gmail.com>
2024-10-28 15:49:49 +0700

    fix(division-seeder): Update division seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
             BranchSeeder::class,
             BrandSeeder::class,
             DepartmentSeeder::class,
+            DivisionSeeder::class,
+            SubDivisionSeeder::class,
         ]);
     }
 }
--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
+use App\Models\Company;
+use App\Models\Department;
 use App\Models\Division;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,24 @@ class DivisionSeeder extends Seeder
      */
     public function run(): void
     {
-        Division::factory(10)->create();
+        $company = Company::first();
+        $branch = Branch::first();
+        $department = Department::first();
+
+        Division::create([
+            'company_id' => $company->id,
+            'branch_id' => $branch->id,
+            'department_id' => $department->id,
+            'company_code' => $company->code,
+            'company_name' => $company->name,
+            'branch_code' => $branch->code,
+            'branch_name' => $branch->name,
+            'department_code' => $department->code,
+            'department_name' => $department->name,
+            'code' => 'DIV001',
+            'name' => 'Division A',
+            'description' => fake()->text(100),
+            'created_by' => 1,
+        ]);
     }
 }
