87b505c565e1f37001faefe2d6320c55302ef0e5
Mahatma Mahardhika <mahatmamahardhika200588@gmail.com>
2024-10-28 15:50:03 +0700

    fix(sub-division-seeder): Update sub division seeder

--- database/seeders/SubDivisionSeeder.php
+++ database/seeders/SubDivisionSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
+use App\Models\Company;
+use App\Models\Department;
+use App\Models\Division;
 use App\Models\SubDivision;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +16,29 @@ class SubDivisionSeeder extends Seeder
      */
     public function run(): void
     {
-        SubDivision::factory(5)->create();
+        $company = Company::first();
+        $branch = Branch::first();
+        $department = Department::first();
+        $division = Division::first();
+
+        SubDivision::create([
+            'company_id' => $company->id,
+            'branch_id' => $branch->id,
+            'department_id' => $department->id,
+            'division_id' => $division->id,
+            'company_code' => $company->code,
+            'company_name' => $company->name,
+            'branch_code' => $branch->code,
+            'branch_name' => $branch->name,
+            'department_code' => $department->code,
+            'department_name' => $department->name,
+            'division_code' => $division->code,
+            'division_name' => $division->name,
+            'code' => 'SDIV001',
+            'name' => 'Sub Division A',
+            'description' => fake()->text(100),
+            'created_by' => 1,
+        ]);
+
     }
 }
