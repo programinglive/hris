fc3af113a671a252912b5849b8aa5a2d9ae612b6
Mahatma Mahardhika <mahatmamahardhika200588@gmail.com>
2025-04-06 01:33:13 +0700

    refactor: remove unused HandleAppearance middleware import
    - Remove unused import for HandleAppearance middleware
    - Clean up imports in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,10 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->encryptCookies(except: ['appearance']);
 
         $middleware->web(append: [
-            HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
